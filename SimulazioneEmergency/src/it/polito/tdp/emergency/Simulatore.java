5bc65049901ddada82e7174dfd5b2977b431496f
Daniele Bianco <Daniele Bianco@172.22.57.29>
2018-05-29 17:52:37 +0200

    Da controllare

--- SimulazioneEmergency/src/it/polito/tdp/emergency/Simulatore.java
+++ SimulazioneEmergency/src/it/polito/tdp/emergency/Simulatore.java
@@ -22,33 +22,94 @@ public class Simulatore {
 	private int TIMEOUT_WHITE = 120;
 	private int TIMEOUT_YELLOW = 60;
 	private int TIMEOUT_RED = 90;
+	private int T_POLLING = 5;
 		
 	// Modello del mondo
 	List<Paziente> pazienti;	
-	StatoPaziente statoTriage;
-	
+	private StatoPaziente statoTriage;
+	int studi_occupati ;
+	private PriorityQueue<Paziente> attesa;
 	// Valori in output
 	private int paz_curati;
 	private int paz_abbandonati;
 	private int paz_morti;
 	
 	// Coda degli eventi
-	PriorityQueue<Event> queue = new PriorityQueue<>();
+	private PriorityQueue<Event> queue = new PriorityQueue<>();
 	
 	public void init() {
 		this.pazienti = new ArrayList<>();
+		this.attesa = new PriorityQueue<>(new PazienteComparator());
 		LocalTime ora = T_inizio;
 		for(int i=0; i<NP; i++) {
 			Paziente p = new Paziente(i+1, StatoPaziente.NEW, ora);
 			Event e = new Event(ora, EventType.ARRIVA, p);
 			ora = ora.plusMinutes(T_ARRIVAL);
+			queue.add(e);
 		}
+		queue.add(new Event(T_inizio, EventType.POLLING,null));
+		
+		this.studi_occupati = 0;
+		
 		statoTriage = StatoPaziente.WHITE;
+		
 		paz_curati = 0;
 		paz_abbandonati = 0;
 		paz_morti = 0;
 	}
 	
+	public int getPaz_curati() {
+		return paz_curati;
+	}
+
+	public int getPaz_abbandonati() {
+		return paz_abbandonati;
+	}
+
+	public int getPaz_morti() {
+		return paz_morti;
+	}
+
+	public void setNS(int nS) {
+		NS = nS;
+	}
+
+	public void setNP(int nP) {
+		NP = nP;
+	}
+
+	public void setT_ARRIVAL(int t_ARRIVAL) {
+		T_ARRIVAL = t_ARRIVAL;
+	}
+
+	public void setT_inizio(LocalTime t_inizio) {
+		T_inizio = t_inizio;
+	}
+
+	public void setT_fine(LocalTime t_fine) {
+		T_fine = t_fine;
+	}
+
+	public void setDURATION_TRIAGE(int dURATION_TRIAGE) {
+		DURATION_TRIAGE = dURATION_TRIAGE;
+	}
+
+	public void setDURATION_WHITE(int dURATION_WHITE) {
+		DURATION_WHITE = dURATION_WHITE;
+	}
+
+	public void setDURATION_YELLOW(int dURATION_YELLOW) {
+		DURATION_YELLOW = dURATION_YELLOW;
+	}
+
+	public void setDURATION_RED(int dURATION_RED) {
+		DURATION_RED = dURATION_RED;
+	}
+
+	public void setT_POLLING(int t_POLLING) {
+		T_POLLING = t_POLLING;
+	}
+
 	public void run() {
 		Event e ;
 		while((e = queue.poll()) != null) {
@@ -82,6 +143,7 @@ public class Simulatore {
 				queue.add(new Event(e.getOra().plusMinutes(TIMEOUT_RED), 
 						EventType.TIMEOUT_RED, e.getPaziente()));
 			}
+			attesa.add(e.getPaziente());
 			
 			//cambiaStatoTriage
 			if(statoTriage==StatoPaziente.WHITE)
@@ -94,27 +156,77 @@ public class Simulatore {
 			break;
 		
 		case CHIAMATA:
+			attesa.remove(e.getPaziente());
+			studi_occupati++;
 			
+			switch(e.getPaziente().getStato()) {
+			case WHITE:
+				queue.add(new Event(e.getOra().plusMinutes(DURATION_WHITE),
+						EventType.USCITA, e.getPaziente())) ;
+				break;
+			case YELLOW:
+				queue.add(new Event(e.getOra().plusMinutes(DURATION_YELLOW),
+						EventType.USCITA, e.getPaziente())) ;
+				break;
+			case RED:
+				queue.add(new Event(e.getOra().plusMinutes(DURATION_RED),
+						EventType.USCITA, e.getPaziente())) ;
+				break;
+			default:
+				break;
+			}
+			
+			e.getPaziente().setStato(StatoPaziente.TREATING);
 			break;
 		
 		case USCITA:
 			// Registrare l'uscita di e.getPaziente()
+			e.getPaziente().setStato(StatoPaziente.OUT);
+			paz_curati++;
+			studi_occupati--;
 			
 			// Decidere chi deve essere chiamato (sulla base del colore e di ora di arrivo)
 			// Tra i pazienti in attesa
-			
+			Paziente paz = attesa.peek();
+			if(paz != null) {
+				queue.add(new Event(e.getOra(),EventType.CHIAMATA,paz)) ;
+			}
 			// Schedulare per ADESSO la CHIAMATA del paziente
 			break;
 		
 		case TIMEOUT_WHITE:
+			if(e.getPaziente().getStato()==StatoPaziente.WHITE) {
+				attesa.remove(e.getPaziente());
+				paz_abbandonati++;
+				e.getPaziente().setStato(StatoPaziente.OUT);
+			}
 			break;
 		
 		case TIMEOUT_YELLOW:
+			if(e.getPaziente().getStato()==StatoPaziente.YELLOW) {
+				attesa.remove(e.getPaziente());
+				e.getPaziente().setStato(StatoPaziente.RED);
+				queue.add(new Event(e.getOra().plusMinutes(TIMEOUT_RED), 
+						EventType.TIMEOUT_RED, e.getPaziente()));
+				attesa.add(e.getPaziente()); // ora è in una posizione privilegiata risp a prima
+			}
 			break;
 		
 		case TIMEOUT_RED:
+			if(e.getPaziente().getStato()==StatoPaziente.RED) {
+				attesa.remove(e.getPaziente());
+				paz_morti++;
+				e.getPaziente().setStato(StatoPaziente.DEAD);
+			}
 			break;
-			
+		
+		case POLLING:
+			if(studi_occupati<NS && !attesa.isEmpty()) {
+				Paziente paz2 = attesa.peek();
+				queue.add(new Event(e.getOra(),EventType.CHIAMATA,paz2)) ;
+			}
+			queue.add(new Event(e.getOra().plusMinutes(T_POLLING),EventType.POLLING, null));
+			break;
 		}
 		
 	}
--- SimulazioneEmergency/src/it/polito/tdp/emergency/TestSimulatore.java
+++ SimulazioneEmergency/src/it/polito/tdp/emergency/TestSimulatore.java
@@ -3,7 +3,18 @@ package it.polito.tdp.emergency;
 public class TestSimulatore {
 
 	public static void main(String[] args) {
+		Simulatore sim = new Simulatore();
 		
+		sim.setNP(200);
+		sim.setNS(5);
+		sim.setT_ARRIVAL(5);
+		
+		sim.init();
+		sim.run();
+		
+		System.out.println("Pazienti curati: " + sim.getPaz_curati());
+		System.out.println("Pazienti abbandonati: " + sim.getPaz_abbandonati());
+		System.out.println("Pazienti morit: " + sim.getPaz_morti());
 	}
 
 }
